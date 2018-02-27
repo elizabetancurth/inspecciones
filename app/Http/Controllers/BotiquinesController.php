c075d1d33cad549ac41ac49f31007dacb2bb04b5
Elizabeth Betancurth <elizabetancurth@gmail.com>
2018-02-27 16:12:18 -0500

    Configuración de la vista para listar boquines y crear botiquines

--- app/Http/Controllers/BotiquinesController.php
+++ app/Http/Controllers/BotiquinesController.php
@@ -13,7 +13,7 @@ class BotiquinesController extends Controller
      */
     public function index()
     {
-        //
+        return view('botiquines.consultar');
     }
 
     /**
@@ -23,7 +23,7 @@ class BotiquinesController extends Controller
      */
     public function create()
     {
-        //
+        return view('botiquines.crear');
     }
 
     /**
