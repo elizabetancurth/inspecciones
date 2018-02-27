453cde8638a8ffbd665a9dbc6f5cfce9fb4f8d81
Elizabeth Betancurth <elizabetancurth@gmail.com>
2018-02-27 07:37:48 -0500

    Configuración de página bienvenida

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,10 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-<div class="topnav" id="myTopnav">
-    <a href="#home" class="active">Home</a>
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
-    <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-    </div>
-</div>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Bienvenido</div>
-
+                <div class="encabezado">Bienvenido</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,62 +15,93 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <div id="bar-menu">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        <img src="images/logo.png" alt="" width=150px;>
-                    </a>
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        Inspecciones | Unidad de Seguridad y Salud en el Trabajo
-                    </a>
-                </div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <div class="row">
+        <div class="col-md-9">
+            <div id="app">
+                <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+                    <div class="container">
+                        <div id="bar-menu">
+                            <a class="navbar-brand" href="{{ url('/') }}">
+                                <img src="images/logo.png" alt="" width=150px;>
+                            </a>
+                            <a class="navbar-brand" href="{{ url('/') }}">
+                                Inspecciones | Unidad de Seguridad y Salud en el Trabajo
+                            </a>
+                        </div>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <!-- Left Side Of Navbar -->
+                            <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                            </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('register') }}">Registrarse</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                            <!-- Right Side Of Navbar -->
+                            <ul class="navbar-nav ml-auto">
+                                <!-- Authentication Links -->
+                                @guest
+                                    <li><a class="nav-link" href="{{ route('register') }}">Registrarse</a></li>
+                                @else
+                                    <li class="nav-item dropdown">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            {{ Auth::user()->name }} <span class="caret"></span>
+                                        </a>
 
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        Cerrar sesión
-                                    </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();">
+                                                Cerrar sesión
+                                            </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    </li>
+                                @endguest
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+                <!-- Menú de navegación usuarios autenticados -->
+                @if (Auth::user())
+                    <div class="py-4">
+                        <div class="col-menu">
+                            <div class="col-md-4">
+                                <div class="container">    
+                                    <div class="encabezado">
+                                        Menú
+                                    </div>    
+                                    <div class="card">
+                                        <nav class="navbar navbar-default">
+                                            <div class="container-fluid">
+                                                <ul class="nav nav-pills nav-stacked">
+                                                    <li><a href="#">Getión de insumos</a></li>
+                                                    <li><a href="#">Gestión de inspeccines</a></li>
+                                                    <li><a href="#">Gestión de BPM </a></li>
+                                                    <li><a href="#">Consultar histórico</a></li>
+                                                    <li><a href="#">Reportes</a></li>
+                                                </ul>
+                                            </div>
+                                        </nav>
+                                    </div>                  
+                                </div>    
+                            </div>
+                        </div>
+                    </div>
+                @endif
+
+                <main class="py-4">
+                    @yield('content')
+                </main>
             </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        </div>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}"></script>
     </div>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
