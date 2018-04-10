1d620f62c2da946c4cea3bc477d8839cb06e1153
Daniel Arboleda <daniel967.28@gmailcom>
2018-04-10 13:34:04 -0500

    Actualización Layout del Login.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
                     <label for="email" class="col-md-4   text-md-right">
                         Usuario:
                     </label>
-                    <div class="col-md-6">
+                    <div>
                         <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                             @if ($errors->has('email'))
@@ -31,7 +31,7 @@
                 <div class="form-group row">
                     <label for="password" class="col-md-4 col-form-label text-md-right">Contraseña:</label>
 
-                    <div class="col-md-6">
+                    <div>
                         <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
                         @if ($errors->has('password'))
                             <span class="invalid-feedback">
