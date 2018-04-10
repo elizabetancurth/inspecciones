b6c98539eebbee2ea614ad205859a0c7f7ed6030
Daniel Arboleda <daniel967.28@gmailcom>
2018-04-10 13:30:23 -0500

    Modificación del Layout del Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,63 +1,69 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section("headerTitle", "Ingreso al sistema")
 
 @section('content')
-<div class="col col-md-10">
-    <div class="row justify-content-center">
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
 
-            <div class="form-group row">
-                <label for="email" class="col-sm-4 col-form-label text-md-right">
-                    Usuario:
-                </label>
-                <div class="col-md-6">
-                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+    <div class="card">
+        <div class="card-header">
+           Inicio de Sesión
+        </div>
+        <div class="card-body">
+            <div class="row justify-content-center container-fluid">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                </div>
-            </div>
+                <div class="form-group row">
+                    <label for="email" class="col-md-4   text-md-right">
+                        Usuario:
+                    </label>
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
-            <div class="form-group row">
-                <label for="password" class="col-md-4 col-form-label text-md-right">Contraseña:</label>
-
-                <div class="col-md-6">
-                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-                    @if ($errors->has('password'))
-                        <span class="invalid-feedback">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                </div>
-            </div>
-
-            <div class="form-group row">
-                <div class="col-md-6 offset-md-4">
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Recordarme
-                        </label>
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
                     </div>
                 </div>
-            </div>
 
-            <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                    <button type="submit" class="btn btn-primary">
-                        Iniciar sesión
-                    </button>
+                <div class="form-group row">
+                    <label for="password" class="col-md-4 col-form-label text-md-right">Contraseña:</label>
 
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        Olvidaste tu contraseña?
-                    </a>
+                    <div class="col-md-6">
+                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                    </div>
                 </div>
-            </div>
-        </form>               
+
+                <div class="form-group row">
+                    <div class="col-md-6 offset-md-4">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Recordarme
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group row mb-0">
+                    <div class="col-md-8 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
+                            Iniciar sesión
+                        </button>
+
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            Olvidaste tu contraseña?
+                        </a>
+                    </div>
+                </div>
+            </form>               
+        </div>
+        </div>
     </div>
-</div>
 @endsection
---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" integrity="sha256-BJ/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    </head>
+</head>
+<body>
+    <!-- Header -->
+        
+    <div class="card-body container-fluid text-center align-middle">
+            <div id="bar-menu">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="images/logo.png" alt="" width=100px;>
+                </a>
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <h1>Unidad de Seguridad y Salud en el Trabajo</h1>
+                </a>
+            </div>  
+        </div>
+        
+        <!-- End of Header -->
+
+        <div class="container col-md-5">
+            @yield("content")
+        </div>
+
+        <div class="card-body container-fluid text-center align-middle footer" style="padding-top: 15px;">
+            <p>
+                <h6>
+                    Universidad Autónoma de Manizales<br> 
+                    ELizabeth Betancurth Candamil, Daniel Arboleda Betancur y Jorge Iván Meza Martínez<br>
+                    Semillero UAM <br>
+                    Derechos Reservados 2018
+                </h6>
+            </p>
+        </div>
+</body>
+</html>
