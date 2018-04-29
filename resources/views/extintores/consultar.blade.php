83b79d3ce2eebdc1a2c8cccf56e751e52a387144
Daniel Arboleda <daniel967.28@gmailcom>
2018-04-28 21:35:13 -0500

    [EXTINTORES] Búsqueda de un extintor por página y código.

--- resources/views/extintores/consultar.blade.php
+++ resources/views/extintores/consultar.blade.php
@@ -4,13 +4,36 @@
 
 @section("content")
 
+    <script>
+            function myFunction() {
+            // Declare variables 
+            var input, filter, table, tr, td, i;
+            input = document.getElementById("buscar");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("tabla");
+            tr = table.getElementsByTagName("tr");
+            
+            // Loop through all table rows, and hide those who don't match the search query
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[1];
+                if (td) {
+                if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+                } 
+            }
+            }
+    </script>
+
     <a class="btn btn-info container-fluid" href="{{ route('extintores.create')}}" role="button">Crear</a>
     <hr>
 
     <!--<div class="align-items-end text-right container-fluid input-group mb-3 col-md-4">-->
     <div class="container-fluid input-group mb-3" style="padding: 0px;">
         <div class="col-md-8"><h2>Listado General de Extintores</h2></div>
-        <input type="text" class="form-control" placeholder="Buscar..." aria-label="Buscar" aria-describedby="basic-addon2">
+        <input onkeyup="myFunction()" id="buscar" type="text" class="form-control" placeholder="Buscar Código..." aria-label="Buscar" aria-describedby="basic-addon2">
         <div class="input-group-append">
             <button class="btn btn-outline-secondary" type="button">
                     <a class="text-muted" href="#"><span class="oi oi-magnifying-glass"></span></a>
@@ -20,7 +43,7 @@
     <hr>
 
     @if(count($extintores) >0)
-        <table class="table">
+        <table id="tabla" class="table">
             <thead class="thead-dark">
             <tr>
                 <th scope="col">Código</th>
