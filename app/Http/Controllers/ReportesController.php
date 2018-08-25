89f58d9a20ce3571ce588884f58183dbddc8f015
Elizabeth Betancurth <elizabetancurth@gmail.com>
2018-08-25 18:01:23 -0500

    Configuración de reporte extintores

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Validation\ValidationRequests;
+
+use Session;
+use App\Formato;
+use App\Extintor;
 
 class ReportesController extends Controller
 {
     public function reporte_extintores()
     {
-        return view('reportes.extintores');
+        $formato = Formato::findOrFail(1);
+        $extintores = Extintor::where('estado', 'Activo')->get();
+        return view('reportes.extintores', ['formato' => $formato, 'extintores' => $extintores]);
     }
 }
--- resources/views/reportes/extintores.blade.php
+++ resources/views/reportes/extintores.blade.php
@@ -5,5 +5,43 @@
 @section("content")
 
 <a href="/home">< Volver a página principal</a>
+<br><br>
+<h4>Seleccione periodo:</h4>
+<hr>
+<div class="table table-light">
+    <div class="thead-light">
+        <div class="row"> 
+            <div class="col col-md-3"> 
+                {{ Form::label('desde', 'Desde: ') }} 
+                {{ Form::date('fecha_desde', \Carbon\Carbon::now(), ['class' => 'form-control']) }}
+            </div>
+            <div class="col col-md-3"> 
+                {{ Form::label('hasta', 'Hasta: ') }} 
+                {{ Form::date('fecha_hasta', \Carbon\Carbon::now(), ['class' => 'form-control']) }}
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+
+<table class="table">
+    <thead class="thead-light">
+        <tr>
+            <th scope="col">Extintor</th>
+            @foreach($formato->preguntas as $pregunta)
+                <th scope="col">{{$pregunta -> descripcion}}</th>
+            @endforeach
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($extintores as $extintor)
+        <tr>
+            <td>{{$extintor->codigo}} </td>
+            <td></td>
+            <td></td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
 
 @endsection
