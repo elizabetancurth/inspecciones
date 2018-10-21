8a7a477d5fda94238b3ed166618bb658588ca09a
Elizabeth Betancurth <elizabetancurth@gmail.com>
2018-10-20 22:55:02 -0500

    Generar reporte de inspección de extintores por rango de fecha

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -30,17 +30,33 @@ class ReportesController extends Controller
         $fecha_desde = $input['fecha_desde'];
         $fecha_hasta = $input['fecha_hasta'];
 
-       $inspecciones = DB::table('inspecciones')->join('respuestas_inspecciones', 'inspecciones.id', '=', 'respuestas_inspecciones.inspeccion_id')
+       $extintores_inspeccionados = DB::table('inspecciones')
                     ->join('inspecciones_extintores', 'inspecciones.id', '=', 'inspecciones_extintores.inspeccion_id')
-                    ->join('formatos_inspecciones', 'formatos_inspecciones.id', '=',  'inspecciones.formato_inspeccion_id')
                     ->join('extintores', 'extintores.id', '=', 'inspecciones_extintores.extintor_id')
                     ->join('ubicaciones', 'ubicaciones.id', '=', 'extintores.ubicacion_id')
                     ->join('edificios', 'edificios.id', '=', 'ubicaciones.edificio_id')
-                    ->join('preguntas_formatos', 'preguntas_formatos.formato_inspeccion_id', '=', 'formatos_inspecciones.id')
                     ->join('clasificaciones_extintores', 'clasificaciones_extintores.id', '=', 'extintores.clasificacion_extintor_id')
+                    ->where([['inspecciones.fecha', '>=', $fecha_desde],['inspecciones.fecha', '<=', $fecha_hasta],['inspecciones.estado_inspeccion', 'Realizada']])
         ->select('extintores.codigo', 'clasificaciones_extintores.nombre as clasificacion', 'extintores.capacidad', 'edificios.nombre', 'ubicaciones.piso', 'ubicaciones.referencia',
-                    'extintores.altura', 'preguntas_formatos.descripcion', 'respuestas_inspecciones.respuesta', 'respuestas_inspecciones.observaciones')->get();
+                     'extintores.altura', 'inspecciones.formato_inspeccion_id', 'inspecciones_extintores.inspeccion_id')->get();
 
-        return view('reportes.ver_reporte_extintores', ['inspecciones' => $inspecciones]);
+        if(count( $extintores_inspeccionados) > 0)
+        {
+            $preguntas = DB::table('preguntas_formatos')
+                        ->where('formato_inspeccion_id', $extintores_inspeccionados[0]->formato_inspeccion_id)
+                        ->select('descripcion')->get();
+
+            $respuesta_inpecciones = DB::table('inspecciones')
+                        ->join('inspecciones_extintores', 'inspecciones.id', '=', 'inspecciones_extintores.inspeccion_id')
+                        ->join('formatos_inspecciones', 'formatos_inspecciones.id', '=', 'inspecciones.formato_inspeccion_id')
+                        ->join('preguntas_formatos', 'preguntas_formatos.formato_inspeccion_id', '=', 'formatos_inspecciones.id')
+                        ->join('respuestas_inspecciones', [['respuestas_inspecciones.inspeccion_id', '=', 'inspecciones.id'], ['respuestas_inspecciones.pregunta_formato_id', '=', 'preguntas_formatos.id']])
+                        ->where([['inspecciones.fecha', '>=', $fecha_desde],['inspecciones.fecha', '<=', $fecha_hasta], ['preguntas_formatos.formato_inspeccion_id', $extintores_inspeccionados[0]->formato_inspeccion_id], ['inspecciones.estado_inspeccion', 'Realizada']])           
+            ->select('respuestas_inspecciones.respuesta', 'respuestas_inspecciones.observaciones', 'inspecciones_extintores.inspeccion_id')->get();                      
+
+            return view('reportes.ver_reporte_extintores', ['extintores_inspeccionados' => $extintores_inspeccionados, 'preguntas' => $preguntas, 'respuesta_inpecciones' => $respuesta_inpecciones]);
+        }
+
+        return view('reportes.ver_reporte_extintores', ['extintores_inspeccionados' => $extintores_inspeccionados]);
     }
 }
--- resources/views/reportes/ver_reporte_extintores.blade.php
+++ resources/views/reportes/ver_reporte_extintores.blade.php
@@ -4,34 +4,55 @@
 
 @section("content")
 
+<a href="{{ route('reporte_inspecciones.extintores') }}">< Volver</a>
+<br>
+
+@if(count($extintores_inspeccionados) > 0)
 <table class="table">
     <thead class="thead-light">
         <tr>
-            <th scope="col">Extintor</th>
-            <th scope="col">Clasificación</th>
-            <th scope="col">Capacidad</th>
-            <th scope="col">Bloque</th>
-            <th scope="col">Ubicación</th>
-            <th scope="col">Altura (cm)</th>
-            <!-- @foreach($inspecciones as $pregunta)
-                <th scope="col">{{$pregunta -> descripcion}}</th>
-            @endforeach -->
+            <th scope="col" class="align-middle">Extintor</th>
+            <th scope="col" class="align-middle">Clasificación</th>
+            <th scope="col" class="align-middle">Capacidad</th>
+            <th scope="col" class="align-middle">Bloque</th>
+            <th scope="col" class="align-middle">Piso</th>
+            <th scope="col" class="align-middle">Ubicación</th>
+            <th scope="col" class="align-middle">Altura (cm)</th>
+            @foreach($preguntas as $pregunta)
+                <th scope="col" class="align-middle">{{$pregunta->descripcion}}</th>
+                <th scope="col" class="align-middle">Observación</th>
+            @endforeach
         </tr>
     </thead>
     <tbody>
-    @foreach($inspecciones as $inspeccion)
+    @foreach($extintores_inspeccionados as $inspeccion)
         <tr>
             <td>COD {{$inspeccion->codigo}} </td>
             <td>{{$inspeccion->clasificacion}} </td>
-            <td>{{$inspeccion->capacidad}} </td>
-            <td>{{$inspeccion->nombre}} </td>
-            <td>Piso {{$inspeccion->piso}}, {{$inspeccion->referencia}}</td>
+            <td>{{$inspeccion->capacidad}} lb</td>
+            <td>{{$inspeccion->nombre}}</td>
+            <td>{{$inspeccion->piso}}</td>
+            <td>{{$inspeccion->referencia}}</td>
             <td>{{$inspeccion->altura}}</td>
-            <td>{{$inspeccion->respuesta}}</td>
-            <td></td>
+            @foreach($respuesta_inpecciones as $respuesta)
+                @if($respuesta->inspeccion_id === $inspeccion->inspeccion_id)
+                    <td>{{$respuesta->respuesta}}</td>
+                    @if($respuesta->observaciones === NULL)
+                        <td>--</td>
+                    @else
+                        <td>{{$respuesta->observaciones}}</td>
+                    @endif
+                @endif
+            @endforeach
+            
         </tr>
     @endforeach
     </tbody>
 </table>
+@else
+<div class="alert alert-danger">
+            <strong>¡Atención!</strong> No existen inspecciones en el rango de fecha indicado.
+        </div>
+@endif
 
 @endsection
